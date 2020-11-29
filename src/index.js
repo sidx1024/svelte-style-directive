d5264f11520844046f66482eb9ef92d2e8c2cab9
sidgo <siddharth.goswami@razorpay.com>
2020-11-29 19:39:09 +0530

    Refactor to reduce code

--- src/index.js
+++ src/index.js
@@ -31,59 +31,53 @@ function process({ magicString, content }) {
    * 
    * Before: <div style:color={color} style:width="100px"></div>
    * After:  <div style=`color: ${color}; width: 100px;`></div>
-   */ 
-  const stack = [];
-
+   */
   return {
-    enter(node, parent) {
+    enter(node) {
       if (node.type === NodeType.ELEMENT) {
-        // You entered a new Element (Example: <div>), remember that.
-        stack.unshift({ styles: '' });
-      } else if (node.type === NodeType.ATTRIBUTE && node.name.startsWith('style:')) {
+        const accumulatedStyles = node.attributes.reduce((acc = '', node) => {
+          if (node.type === NodeType.ATTRIBUTE && node.name.startsWith('style:')) {
+            // Extract whatever from style:whatever
+            const property = node.name.split(':')[1];
 
-        if (parent.type !== NodeType.ELEMENT) {
-          throw `Using style directives on ${parent.type} node is not supported`;
-        }
+            let style = '';
+            if (node.value === true) {
+              // IDEs hate that syntax, also you can't use shorthand
+              // for property names which have hypens (background-color)
+              throw 'Shorthand values are not supported.';
+            } else if (node.value[0].type === NodeType.MUSTACHE_TAG) {
+              // It's like style:color={color}
 
-        // Extract whatever from style:whatever
-        const property = node.name.split(':')[1];
+              const expression = node.value[0].expression;
+              const value = content.slice(expression.start, expression.end);
 
-        let style = '';
-        if (node.value === true) {
-          // IDEs hate that syntax, also you can't use shorthand
-          // for property names which have hypens (background-color)
-          throw 'Shorthand values are not supported.';
-        } else if (node.value[0].type === NodeType.MUSTACHE_TAG) {
-          // It's like style:color={color}
+              // Check if the value is truthy or 0, if yes, then include the style.
+              style += `$\{${value} || ${value} !== 0 ? ` +
+                `"${property}: " + ${value} + "; "` + ': ""}';
+            } else if (node.value[0].type === 'Text') {
+              // It's like style:color="red"
 
-          const expression = node.value[0].expression;
-          const value = content.slice(expression.start, expression.end);
+              const value = node.value[0].raw;
+              style += `${property}: ${value};`;
+            } else {
+              throw `Unsupported node: "${node.value[0].type}"`;
+            }
 
-          // Check if the value is truthy or 0, if yes, then include the style.
-          style += `$\{${value} || ${value} !== 0 ? ` + `"${property}: " + ${value} + "; "` + ': ""}';
-        } else if (node.value[0].type === 'Text') {
-          // It's like style:color="red"
+            if (style) {
+              // Remove the original usage of style directive ( style:color={color})
+              magicString.remove(node.start - 1, node.end);
 
-          const value = node.value[0].raw;
-          style += `${property}: ${value};`;
-        } else {
-          throw `Unsupported node: "${node.value[0].type}"`;
-        }
+              acc += style;
+            }
+          }
 
-        // Merge the style for the current Element.
-        stack[0].styles += style;
+          return acc;
+        }, '');
 
-        // Remove the original usage of style directive ( style:color={color})
-        magicString.remove(node.start - 1, node.end);
-      }
-    },
-    leave(node) {
-      if (node.type === NodeType.ELEMENT) {
-        // You're leaving the Element, time to apply the styles.
-        const { styles } = stack[0];
-        if (styles) {
-
-          const styleAttribute = node.attributes.find(node => node.name === 'style');
+        if (accumulatedStyles) {
+          const styleAttribute = node.attributes.find(node =>
+            node.type === NodeType.ATTRIBUTE && node.name === 'style'
+          );
           if (styleAttribute) {
             throw 'Mixing style attribute and style directive is not supported.';
           }
@@ -91,9 +85,7 @@ function process({ magicString, content }) {
           // <div*>
           //     ^ Insert the style attribute just after the tag name.
           const position = node.start + 1 + node.name.length;
-          magicString.appendLeft(position, ` style={\`${styles}\`}`);
-
-          stack.shift();
+          magicString.appendLeft(position, ` style={\`${accumulatedStyles}\`}`);
         }
       }
     },
